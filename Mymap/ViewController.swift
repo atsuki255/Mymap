272bf7944ac64aecd761a706ec9141ba63eb82ee
atsuki225 <atsuki443@gmail.com>
2020-11-24 15:31:13 +0900

    create map app

---
+++ Mymap/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Mymap
+//
+//  Created by Apple on 2020/03/15.
+//  Copyright © 2020 anthing possible. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController , UITextFieldDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
+    }
+
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        if let searchKey = textField.text {
+            
+            print(searchKey)
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks,error)in
+                
+                if let unwrapPlacemarks = placemarks {
+                    
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        
+                        if let location = firstPlacemark.location {
+                            
+                            let targetCoordinate = location.coordinate
+                            
+                            print(targetCoordinate)
+                            
+                            let pin = MKPointAnnotation()
+                            
+                            pin.coordinate = targetCoordinate
+                            
+                            pin.title = searchKey
+                            
+                            self.dispMap.addAnnotation(pin)
+                            
+                            self.dispMap.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+            })
+            
+        }
+        
+        return true
+    }
+    
+    
+    
+}
+
